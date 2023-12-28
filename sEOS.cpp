bb8ad670565c6a64e17aa5a5d85858dcb91912f4
Oussama Kallel <oussama.kallel@gmail.com>
2023-12-28 13:21:57 +0100

    Update sEOS.cpp
    deactivate disabling Systick timer in order that the time measurement is correct for Arduino library using millis() function

--- sEOS.cpp
+++ sEOS.cpp
@@ -7,6 +7,6 @@ void sEOS_Init(TIM_TypeDef *Instance, uint32_t intervall, TimerFormat_t format, 
   MyTim->setOverflow(intervall, format); 
   MyTim->attachInterrupt(callback);//configure the function callback as an interrupt for the Update event
   MyTim->resume();// start Timer
-  HAL_SuspendTick();// Halt systick timer used by HAL library
+  //HAL_SuspendTick();// Halt systick timer used by HAL library
   HAL_PWR_EnableSleepOnExit();// Sleep after intterupt 
 }
